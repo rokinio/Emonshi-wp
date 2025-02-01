fcb39fc5dfe169258054ac73487fa94e5b7af469
zarrinfard <amir.zarrinfard@gmail.com>
2025-02-01 20:34:40 +0330

    feat: add new feature

--- emonshi-wp.php
+++ emonshi-wp.php
@@ -1,17 +1,20 @@
 <?php
 /*
 Plugin Name: Emonshi - Medical Booking System
-Plugin URI: https://emonshi.net
-Description: Official medical appointment booking system for doctors - Simple and modern appointment booking solution
+Plugin URI: https://emonshi.com
+Description: Official medical appointment booking system for doctors
 Version: 1.0.0
 Requires at least: 5.0
 Requires PHP: 7.4
 Author: hamyarsystem
-Author URI: https://emonshi.net
+Author URI: https://emonshi.com
 License: GPL v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: emonshi-wp
 Domain Path: /languages
+GitHub Plugin URI: rokinio/Emonshi-wp
+GitHub Branch: main
+Update URI: false
 */
 // If this file is called directly, abort.
 if (!defined('ABSPATH')) {
@@ -60,4 +63,19 @@ register_deactivation_hook(__FILE__, 'emonshi_booking_deactivate');
 function emonshi_booking_shortcode() {
     return '<div id="emonshi-booking-calendar"></div>';
 }
+
+if (!defined('EMONSHI_UPDATE_SOURCE')) {
+    // Check if plugin is installed from WordPress.org
+    if (false !== strpos(__FILE__, 'wp-content/plugins')) {
+        define('EMONSHI_UPDATE_SOURCE', 'wordpress.org');
+    } else {
+        define('EMONSHI_UPDATE_SOURCE', 'github');
+    }
+}
+
+// Disable GitHub updates if installed from WordPress.org
+if (EMONSHI_UPDATE_SOURCE === 'wordpress.org') {
+    add_filter('github_updater_hide_settings', '__return_true');
+}
+
 add_shortcode('emonshi_booking', 'emonshi_booking_shortcode');
